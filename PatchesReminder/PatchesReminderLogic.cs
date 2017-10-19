1944486a7021c30289563374f66ed4f6314e03d5
Martin Johansson <niteniten58@gmail.com>
2017-10-19 08:44:51 +0200

    Initial commit
    Basic functionality to detect when Patches enter the battlefield, either
    looking at known cards in deck or noticing when he enters from deck

---
+++ PatchesReminder/PatchesReminderLogic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+
+namespace PatchesReminder
+{
+    public class PatchesReminderLogic
+    {
+        //public delegate void PEEventHandler(string cardName);
+        /// <summary>
+        /// Triggers when Patches enters the Battlefield from the Deck
+        /// </summary>
+        public event EventHandler<string> PatchesEnter;
+        /// <summary>
+        /// Triggers when Patches no longer can attack
+        /// </summary>
+        public event EventHandler PatchesAttacked;
+
+        private readonly HashSet<string> _attackers = new HashSet<string>();
+
+        public void PlayerDeckToPlay(Card card)
+        {
+            if (card.Mechanics.Contains("charge", StringComparer.InvariantCultureIgnoreCase))
+            {
+                Log.Info($"Attack with {card.Name}");
+                //Notify player to attack with patches
+                _attackers.Add(card.Name);
+                OnPatchesEnter(card.Name);
+            }
+        }
+
+        public void PlayerMinionAttack(AttackInfo attackInfo)
+        {
+            //Check if Patches still can attack
+            if (_attackers.Contains(attackInfo.Attacker.Name))
+            {
+                _attackers.Remove(attackInfo.Attacker.Name);
+                //If Patches can not, stop telling player to attack with patches
+                OnPatchesAttacked();
+            }
+        }
+
+        public void PlayerPlay(Card card)
+        {
+            //Hearthstone_Deck_Tracker.API.Core.Game.Player.PlayerCardList.Any(c => c.Id == HearthDb.CardIds.Collectible.Neutral.PatchesThePirate)
+            //Hearthstone_Deck_Tracker.API.Core.Game.Player.Deck.Any(e => e.CardId == HearthDb.CardIds.Collectible.Neutral.PatchesThePirate)
+            if (card.Race == HearthDbConverter.RaceConverter(HearthDb.Enums.Race.PIRATE) &&
+                Hearthstone_Deck_Tracker.API.Core.Game.Player.PlayerCardList.Any(c => c.Id == HearthDb.CardIds.Collectible.Neutral.PatchesThePirate && c.Count > 0))
+            {
+                if (Hearthstone_Deck_Tracker.API.Core.Game.Player.Board.Count(e => e.GetTag(HearthDb.Enums.GameTag.ZONE_POSITION) > 0) < 6)
+                {
+                    Card patches = Hearthstone_Deck_Tracker.API.Core.Game.Player.PlayerCardList.First(c => c.Id == HearthDb.CardIds.Collectible.Neutral.PatchesThePirate);
+                    //Hearthstone_Deck_Tracker.API.Core.Game.Player.
+                    _attackers.Add(patches.Name);
+                    OnPatchesEnter(patches.Name);
+                }
+            }
+            //if (Hearthstone_Deck_Tracker.API.Core.Game.Player.Deck.Any(e => e.Card.Name == "Patches the Pirate"))
+
+            //throw new NotImplementedException();
+        }
+
+        protected virtual void OnPatchesEnter(string cardName)
+        {
+            PatchesEnter?.Invoke(this, cardName);
+        }
+
+        protected virtual void OnPatchesAttacked()
+        {
+            PatchesAttacked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PatchesReminder/PatchesReminderPlugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+
+namespace PatchesReminder
+{
+    public class PatchesReminderPlugin : IPlugin
+    {
+        public string Name => "Patches Reminder";
+
+        public string Description => "Reminds you to attack with Patches";
+
+        public string ButtonText => "Settings";
+
+        public string Author => "Martin Johansson";
+
+        public Version Version => new Version(0,0,1);
+
+        public MenuItem MenuItem => null;
+
+        private PatchesReminderLogic _logic;
+        private PatchesReminderDisplay _prd;
+
+        public void OnButtonPress()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnLoad()
+        {
+            _prd = new PatchesReminderDisplay();
+            Core.OverlayCanvas.Children.Add(_prd);
+            _logic = new PatchesReminderLogic();
+            _logic.PatchesAttacked += (sender, args) => _prd.Hide();
+            _logic.PatchesEnter += (sender, cardName) =>
+            {
+                Log.Info($"Attack with {cardName}");
+                //prd.DisplayText = $"ATTACK WITH {cardName.ToUpper()}";
+                _prd.Show();
+            };
+            GameEvents.OnPlayerPlay.Add(_logic.PlayerPlay);
+
+            GameEvents.OnPlayerDeckToPlay.Add(_logic.PlayerDeckToPlay);
+            GameEvents.OnPlayerMinionAttack.Add(_logic.PlayerMinionAttack);
+            GameEvents.OnGameEnd.Add(_prd.Hide);
+            GameEvents.OnTurnStart.Add( a => _prd.Hide());
+        }
+
+        public void OnUnload()
+        {
+            _prd.Hide();
+            //throw new NotImplementedException();
+        }
+
+        public void OnUpdate()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
